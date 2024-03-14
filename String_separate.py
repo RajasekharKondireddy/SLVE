1908c5b2dad1cdab6efcffb8b13cc8f45b6f5fa0
RajasekharKondireddy <sekharaju.k@gmail.com>
2024-03-14 12:22:10 +0530

    Python program

---
+++ String_separate.py
@@ -0,0 +1,15 @@
+ = ("kt", "ls", "mh")
+result = dict(t)
+print(result)
+old_list = ['k', 'j','l', 'i', 'g', 'f', 'e','a','k','b']
+new_list = list(dict.fromkeys(old_list).keys())
+print(new_list)
+#for element in old_list:
+ #   if element not in new_list:
+  #      new_list.append(element)
+       # print(new_list)
+num = [10,40,20,30]
+num.insert(4, num.pop(1))
+print(num)
+str = "macbook"
+print(str[::-1])
